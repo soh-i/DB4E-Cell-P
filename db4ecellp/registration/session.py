d874551687ca2f2f13e18f9a2e94cc3a14d797fa
yukke <soh.ishiguro3914@gmail.com>
2013-09-19 20:56:07 +0900

    Add tRNA class and db save at local file

--- db4ecellp/registration/session.py
+++ db4ecellp/registration/session.py
@@ -1,30 +1,45 @@
-
-__author__ = 'Soh Ishiguro <t10078si@sfc.keio.ac.jp>'
+__program__ = 'session'
 __version__ = '0.0.1'
+__author__ = 'Soh Ishiguro <t10078si@sfc.keio.ac.jp>'
+__copyright__ = ''
+__license__ = ''
 
 from species import species
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
-
+import os.path
+from os import remove
 
 class Mapper(object):
     def __init__(self):
-        self.engine = species.create_engine('sqlite:///:memory:', echo=False)
+        db_path = '../../db/ecoli.sqlite3'
+        if os.path.isfile(db_path):
+            os.remove(db_path)
+            
+        self.engine = species.create_engine('sqlite:///' + db_path, echo=False)
         species.metadata.create_all(self.engine)
         self.Session = species.sessionmaker()
         self.Session.configure(bind=self.engine)
         self.session = self.Session()
 
+    def __destroy_DB(self):
+        self.session.delete()
+
     def __mapping_CDS(self):
-        with open("../../data/CDS_annotation.tbl", "r") as f:
+        with open('../../data/CDS_annotation.tbl', 'r') as f:
             for line in f:
                 (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
                 obj = species.CDS(name, strand, start, end, feature, sequence)
                 self.session.add(obj)
         self.session.commit()
 
-    def __map_tRNA(self):
-        pass
+    def __mapping_tRNA(self):
+        with open('../../data/CDS_annotation.tbl', 'r') as f:
+            for line in f:
+                (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
+                obj = species.tRNA(name, strand, start, end, feature, sequence)
+                self.session.add(obj)
+        self.session.commit()
 
     def __map_rRNA(self):
         pass
@@ -37,6 +52,7 @@ class Mapper(object):
     
     def generate_db(self):
         self.__mapping_CDS()
+        self.__mapping_tRNA()
 
 
 class Query(Mapper):
@@ -53,26 +69,27 @@ class Query(Mapper):
         for row in self.session.query(species.CDS).all():
             all_rec.append(row)
         return all_rec
+        
+    def collect_all_gene_name(self):
+        names = []
+        for record in self.session.query(species.CDS).order_by(species.CDS.name):
+            names.append(record.name)
+        return names
 
-    def collect_gene_by_name(self, gene_name):
+    def find_gene_by_name(self, gene_name):
         for name in self.session.query(species.CDS).filter_by(name=gene_name):
             return name
     
-    def collect_all_annotations_by_strand(self, strand):
+    def collect_annotations_filter_by_strand(self, strand):
         filt_recs = []
         if strand == 1 or strand == -1:
             for rec in self.session.query(species.CDS).filter_by(strand=strand):
                 filt_recs.append(rec)
             return filt_recs
         else:
-            raise ValueError
+            raise RuntimeError, "Invalid argument given [%d], must be -1 or 1" % (strand)
             
-    def collec_all_gene_name(self):
-        names = []
-        for record in self.session.query(species.CDS).order_by(species.CDS.name):
-            names.append(record.name)
-        return names
-
+    
     def include_record_in_region(self, start, end):
         records = []
         for record in self.session.query(species.CDS).filter(species.CDS.start.between(start, end)):
