b85f388f90da4ca7b8004a23338469382b84a762
yukke <soh.ishiguro3914@gmail.com>
2013-09-19 19:55:46 +0900

    splited main function

--- db4ecellp/registration/session.py
+++ db4ecellp/registration/session.py
@@ -92,19 +92,3 @@ class Query(Mapper):
         return seq
 
     
-if __name__ == '__main__':
-    # create Mapper class object
-    query = Query()
-    
-    #print "Mapping CDS table to DB is finished"
-    
-    #print mapper.retrieve_all_records()
-    #print mapper.query_by_all_cds()
-    #print mapper.query_by_gene_name('thrL')
-    #print mapper.query_by_region(189, 12255)
-    #print mapper.query_by_region_with_gene(1,3010)
-    #print mapper.query_by_region_with_seq(1,3010)
-    #print mapper.collect_all_annotations_by_strand(1)
-    print query.include_seq_in_region(21, 2012)
-
-
