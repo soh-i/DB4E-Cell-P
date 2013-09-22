9a9621d35308b3de64c02496cfa73af84e012c8b
yukke <soh.ishiguro3914@gmail.com>
2013-09-22 13:15:25 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    version='0.0.1',
+    description='E-Cell4 P: Database project for whole cell simulation in E. coli',
+    author='',
+    author_email='',
+    url='https://github.com/soh-i/DB4E-Cell-P/',
+    name='db4ecellp',
+    packages=['db4ecellp', 'db4ecellp.session', 'db4ecellp.session.species'],
+    license='',
+)
+
