6512b3f89127f2be03b1de356cb9694581984779
yukke <soh.ishiguro3914@gmail.com>
2013-09-22 14:26:26 +0900

    move into ../

---
+++ query_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+
+from db4ecellp import db4ecellp
+
+
+def query_test():
+
+    # create Mapper class object (generating DB)
+    query = db4ecellp.Query()
+    
+    print query.count_stored_records()
+    #print query.collect_cds_records()
+    print query.find_by_name('thrL')
+    #print query.collect_annotations_filter_by_strand(-1)
+    print query.include_gene_in_region(21, 2012)
+
+
+if __name__ == '__main__':
+    query_test()
