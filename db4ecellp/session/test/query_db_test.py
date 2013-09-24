cb93969055397aabf5e443661591b07e28d5d162
soh-i <soh.ishiguro3914@gmail.com>
2013-09-24 19:48:25 +0900

    Add test for reflection

--- db4ecellp/session/test/query_db_test.py
+++ db4ecellp/session/test/query_db_test.py
@@ -2,14 +2,31 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 import sys
-sys.path.append('../species')
-from species import Species, CDS, tRNA, rRNA, Promoter, Terminator
+sys.path.append('../')
+from species.species import Species, CDS, tRNA, rRNA, Promoter, Terminator
+from session import QueryBuilder
 
+"""
 engine = create_engine('sqlite:///test.db', echo=False)
 metadata = MetaData(bind=engine)
-metadata.reflect() # use reflection
-
+metadata.reflect(bind=engine) # use reflection
 Session = sessionmaker(bind=engine)
 session = Session()
+"""
+#print session.query(CDS).filter_by(name="thrL").all()
 
-print session.query(CDS).filter_by(name="thrL").all()
+
+conf = '../../../conf.ini'
+q = QueryBuilder(conf)
+
+print q.count_stored_records()
+print q.collect_cds_records()[-1]
+print q.collect_all_gene_name()[-1]
+print q.collect_annotations_filter_by_strand(-1)[-1]
+print q.find_by_name("thrL")
+print q.include_gene_in_region(50000,60000)[0]
+print q.include_record_in_region(50000, 60000)[0]
+print q.include_seq_in_region(2032,9059)[-1][1]
+
+
+
